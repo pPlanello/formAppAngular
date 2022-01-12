2580aafd5be7818a223e5562cc69b58360700d71
pPlanello <planello96@gmail.com>
2022-01-12 13:14:48 +0100

    Creating menu with specifics routes

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+interface MenuItem {
+  text: string;
+  route: string;
+}
 @Component({
   selector: 'app-sidemenu',
   templateUrl: './sidemenu.component.html',
   styleUrls: ['./sidemenu.component.css']
 })
-export class SidemenuComponent implements OnInit {
+export class SidemenuComponent {
 
-  constructor() { }
+  templateMenu: MenuItem[] = [];
+  reactiveMenu: MenuItem[] = [];
 
-  ngOnInit(): void {
+  constructor() {
+    this.templateMenu = [
+      { text: 'Básico', route: '/template/basic' },
+      { text: 'Dinámico', route: '/template/dynamic' },
+      { text: 'Switches', route: '/template/switches' },
+    ];
+
+    this.reactiveMenu = [
+      { text: 'Básico', route: '/reactive/basic' },
+      { text: 'Dinámico', route: '/reactive/dynamic' },
+      { text: 'Switches', route: '/reactive/switches' },
+    ]
   }
+
 
 }
